bef1a8ed9d2dcfdb5ad2e92ba9a0a7889dc0dbd3
j444zhan <j444zhan@uwaterloo.ca>
2024-08-27 16:56:24 -0400

    updated

--- client/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <PageTransition>
-      <main>
-        <div className=""></div>
-      </main>
-    </PageTransition>
-  );
-}
--- client/components/PageTransition.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { staggerContainer } from "@/utils/motion";
-
-const PageTransition = () => {
-  return <div className="relative overflow-hidden flex"></div>;
-};
-
-export default PageTransition;
