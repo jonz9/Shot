2df4c76082ada4dce22a7f3cfb2b76dfab63b547
John Zhang <zhangjohn.ca@gmail.com>
2024-08-27 10:44:06 -0400

    setup updates

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,20 +1,66 @@
 import type { Config } from "tailwindcss";
 
-const config: Config = {
+const { nextui } = require("@nextui-org/react");
+
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+const config = {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
+  prefix: "",
   theme: {
+    light: "fff",
+    dark: "000",
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: {},
+        secondary: {},
+      },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+        "3xl": "1760px",
+      },
+    },
+    animation: {
+      spotlight: "spotlight 2s ease .75s 1 forwards",
+    },
+    keyframes: {
+      spotlight: {
+        "0%": {
+          opacity: "0",
+          transform: "translate(-72%, -62%) scale(0.5)",
+        },
+        "100%": {
+          opacity: "1",
+          transform: "translate(-50%,-40%) scale(1)",
+        },
       },
     },
   },
-  plugins: [],
-};
+  plugins: [require("tailwindcss-animate"), nextui(), addVariablesForColors],
+} satisfies Config;
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
+
 export default config;
